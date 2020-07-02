e6b2b20ddf348b788ea8c34839c04df776da7213
unknown <fjmiranda4iuandes.cl>
2020-07-02 14:04:50 -0400

    first commit

---
+++ examenFmiranda/examenFmiranda/jugador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examenFmiranda
+{
+    public class jugador
+    {
+        private int atack_p;
+        private int defense_p;
+        private int number;
+        private bool goalkeeper;
+
+    }
+}
---
+++ examenFmiranda/examenFmiranda/persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examenFmiranda
+{
+    public abstract class persona
+    {
+        protected string name;
+        protected int age;
+        protected string nationality;
+        protected int salary;
+    }
+}
